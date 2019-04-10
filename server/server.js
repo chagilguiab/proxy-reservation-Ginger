c8bb29afbd3493f056d7f8a96550c3a0c3de2a9f
asuluntas <aysunsulun@gmail.com>
2019-04-09 20:48:42 -0700

    Renders all 4 modules but need to fix positioning.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
-const router = require('./router.js');
 
-app.use(express.static(path.join(__dirname, '/../public')));
+app.use('/', express.static(path.join(__dirname, '/../public')));
 
-//app.use('/:restaurantId/:partySize/:date/:time', express.static(path.join(__dirname, '/../public')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
