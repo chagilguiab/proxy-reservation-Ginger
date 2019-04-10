436d6d90b4791bdffc37f789d185ff9b6a4c2182
asuluntas <aysunsulun@gmail.com>
2019-04-10 16:49:13 -0700

    Proxy serves to different id endpoints for each module

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 
 app.use(express.static(path.join(__dirname, '/../public')));
 
+
+
+app.use((req, res) => {
+  const restaurantId = req.path.slice(1).split('/')[0];
+  if (restaurantId === '' || restaurantId) {
+    res.sendFile(path.join(__dirname, '/../public/index.html'));
+  } else {
+    res.send('Can not load restaurant information!').end();
+  }
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
