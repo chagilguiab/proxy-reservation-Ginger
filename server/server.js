bcc16d1e47ee38372b870f1f2b443d5128565af4
asuluntas <aysunsulun@gmail.com>
2019-04-12 12:10:07 -0700

    Renders four components and it's functianal and styled.

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 3000;
 app.use(morgan('dev'));
 app.use(cors());
 
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   next();
@@ -18,15 +17,13 @@ app.use(function(req, res, next) {
 
 app.use('/:id', express.static(path.join(__dirname, '/../public')));
 
+
 app.use('/:id', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
 app.use('/ratings_ambience/:id', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
-app.use(proxy({ target: 'http://localhost:3002', changeOrigin: true }));
+app.use('/reviews/:id', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
 app.use('/restaurants/:id', proxy({ target: 'http://localhost:3003', changeOrigin: true }));
 app.use('/menus/:id', proxy({ target: 'http://localhost:3004', changeOrigin: true }));
 
-app.get('/:id', function(req, res){
-  res.sendFile(path.join(__dirname + `/public/index.html`));
-});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
